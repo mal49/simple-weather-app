c07b2a623eb8f4569dce046f0d0eb8ab2224dd7c
Ikhmal Hanif <ikhmalhanif60@gmail.com>
2026-02-12 22:34:42 +0800

    remove github pages config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/simple-weather-app",
-  assetPrefix: "/simple-weather-app/",
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
